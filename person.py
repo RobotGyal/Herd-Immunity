bbaf2c02b2c34819c557bfb915290c674161bf40
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-10-06 14:03:13 -0700

    Add did_survive_infection()

--- person.py
+++ person.py
@@ -27,7 +27,14 @@ class Person(object):
         '''
         # Only called if infection attribute is not None.
         # TODO:  Finish this method. Should return a Boolean
-        pass
+        if random.random() > self.infection.mortality_rate:
+            self.is_vaccinated = True
+            self.infection = None
+            return True
+        else:
+            self.is_alive = False
+            return False
+
 
 
 ''' These are simple tests to ensure that you are instantiating your Person class correctly. '''
