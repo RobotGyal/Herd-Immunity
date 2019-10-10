8c66304218562d2d0ecdae899f96a9412887178a
Robotgyal <aleiaknight@gmail.com>
2019-10-10 14:31:57 -0700

    added test file skeletion

---
+++ test_simulation.py
@@ -0,0 +1,23 @@
+import pytest
+import simulation
+
+def test_init():
+    pass
+
+def test_create_population():
+    pass
+
+def test_simulation_should_continue():
+    pass
+
+def test_run():
+    pass
+
+def test_time_step():
+    pass
+
+def test_interaction():
+    pass
+
+def test_infect_newly_infected():
+    pass
